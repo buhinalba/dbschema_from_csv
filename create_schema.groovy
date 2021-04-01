0cd579831efb75213613cc2e06624ae7f30b4a97
buhinalba <buhin.alba@gmail.com>
2021-04-01 21:13:21 +0200

    feat: create constraints (foreign key)

--- create_schema.groovy
+++ create_schema.groovy
@@ -1,53 +1,69 @@
-"""
-Query to check if table exists
-	SELECT EXISTS (
-		SELECT FROM information_schema.tables 
-		WHERE  table_schema = 'public'
-		AND    table_name   = 'test_table1');
+void createSchemaFromCSV(filePath, foreignKeys) {
+	int rowCount = 0;
+	new File(filePath).eachLine { line ->
+		String[] columnData = line.split(',')
+		print rowCount
+		println Arrays.toString(columnData)
+		if (rowCount++ == 0) return;
 
-Create Table query
-	CREATE TABLE table_name (column_name datatype);
+		try {
+			String tableName = columnData[0]
+			String columnName = columnData[1]
+			String type = columnData[2]
+			String length = columnData[3]
+			String primaryKey = columnData[4] == 'Yes' ? 'PRIMARY KEY' : ''
+			String nullable = columnData[5] == 'No' ? 'NOT NULL' : ''
+			String unique = columnData[6] == 'Yes' ? 'UNIQUE' : ''
 
-Add new column query (If Table Exists ) 
-	ALTER TABLE table_name ADD column_name datatype;
-"""
+			def tableExists = sql.rows("SELECT EXISTS ( SELECT FROM information_schema.tables WHERE  table_schema = 'public' AND table_name = ${tableName});")
+			if (tableExists[0].exists) {
+				// todo : when int needs length resricition, it works differently
+				sql.execute("ALTER TABLE \"" + tableName + "\" ADD " + columnName + " " + type + " " + primaryKey + " " + nullable + " " + unique + ";")
+				println "Table ${tableName} Exists!"
+			} else {
+				sql.execute("CREATE TABLE \"" + tableName + "\" (" + columnName + " " + type + " " + primaryKey + " " + nullable + " " + unique + ");")
+			}
 
-
-
-
-
-
-
-
-int rowCount = 0;
-new File('/home/buhinalba/Downloads/talajdb_schema.csv').eachLine { line ->
-	String[]columnData = line.split(',')
-	print rowCount
-	println Arrays.toString(columnData)
-	if (rowCount++ == 0) return;
-
-	try {
-		String tableName = columnData[0]
-		String columnName = columnData[1]
-		String type = columnData[2]
-		String length = columnData[3]
-		String primaryKey = columnData[4] == 'Yes' ? 'PRIMARY KEY' : ''
-		String nullable = columnData[5] == 'No' ? 'NOT NULL' : ''
-		String unique = columnData[6] == 'Yes' ? 'UNIQUE' : ''
-
-		def tableExists = sql.rows("SELECT EXISTS ( SELECT FROM information_schema.tables WHERE  table_schema = 'public' AND table_name = ${tableName});")
-		if (tableExists[0].exists) {
-			// todo : when int needs length resricition, it works differently
-			sql.execute("ALTER TABLE \"" + tableName + "\" ADD " + columnName + " " + type + " " + primaryKey + " " + nullable + " " + unique +";")
-			println "Table ${tableName} Exists!"
-		} else {
-			sql.execute("CREATE TABLE \"" + tableName + "\" (" + columnName + " " + type +" " + primaryKey + " " + nullable + " " + unique + ");")
+			checkForeignKey(columnData, foreignKeys)
+		} catch (Exception e) {
+			println e.getMessage()
+			println e.getStackTrace().toString()
+			return
 		}
-	} catch (Exception e) {
-		println e.getMessage()
-		println e.getStackTrace().toString()
-		// todo add row index to new array with error message
 	}
-	rowCount++
-	// println Arrays.toString(data)
 }
+
+
+void checkForeignKey(columnData, foreignKeys) {
+	if (columnData[7] == "Yes") {
+		foreignKeys.add(columnData)
+	}
+}
+
+
+
+void createConstraints(foreignKeys) {
+	println ""
+	println "FOREIGN KEYS: "
+	for (foreignKey: foreignKeys) {
+		println Arrays.toString(foreignKey)
+		String tableName = foreignKey[0]
+		String columnName = foreignKey[1]
+		String referenceTable = foreignKey[9]
+		String referenceColumn = foreignKey[10]
+		String constraintName = "fk_${referenceTable}_${referenceColumn}"
+		String query = "ALTER TABLE ONLY ${tableName} ADD CONSTRAINT ${constraintName} FOREIGN KEY (${columnName}) REFERENCES ${referenceTable}(${referenceColumn});"
+		sql.execute(query)
+	}
+}
+
+
+void main() {
+	String FILE_PATH = "/home/buhinalba/Projects/data_engineering/dbschema_from_csv/movies_schema"
+	def foreignKeys = []
+	createSchemaFromCSV(FILE_PATH, foreignKeys)
+	createConstraints(foreignKeys)
+}
+
+
+main()
